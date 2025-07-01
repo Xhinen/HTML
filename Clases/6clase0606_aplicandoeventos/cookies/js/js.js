e03c2bb581e215e2124ffd197358f03b9fa46d65
Xhinen <xhinen@proton.me>
2025-07-01 12:38:51 +0200

    actualizacion

--- Clases/6clase0606_aplicandoeventos/cookies/js/js.js
+++ Clases/6clase0606_aplicandoeventos/cookies/js/js.js
@@ -2,12 +2,10 @@ const popup = document.getElementById("cookiePopup");
 const btn = document.getElementById("acceptCookies");
 //Cuando no hay cookies aceptadas
 if(!localStorage.cookiesAccepted){
-    popup.classList.add("show")
+    popup.classList.add("show");
 }
 
 btn.addEventListener("click", function(){
-
-   localStorage.cookiesAccepted = "true";
-   popup.classList.remove("show");
-
+    localStorage.cookiesAccepted = "true";
+    popup.classList.remove("show");
 });
--- Clases/6clase0606_aplicandoeventos/menuResposive/contactos/js/js.js
+++ Clases/6clase0606_aplicandoeventos/menuResposive/contactos/js/js.js
@@ -4,12 +4,14 @@ let despliegaClase = "menu-desplegado";
 
 function nav(){
     let lanz = document.querySelector(lanzador);
-    lanz.addEventListener("click", despliegaMenu);
+    lanz.addEventListener("click",despliegaMenu);
+
 }
 
 function despliegaMenu(){
     let despl = document.querySelector(desplegable);
     despl.classList.toggle(despliegaClase);
 }
+
 
 nav();
